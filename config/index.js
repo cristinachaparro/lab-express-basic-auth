e138d3c444274b3ab326dddf24d3caaeacf4f907
Cristina Chaparro <cricbau@gmail.com>
2023-02-15 17:54:42 +0100

    iteration 2 + bonus done

--- config/index.js
+++ config/index.js
@@ -17,6 +17,9 @@ const favicon = require("serve-favicon");
 // https://www.npmjs.com/package/path
 const path = require("path");
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
@@ -35,5 +38,21 @@ module.exports = (app) => {
   app.use(express.static(path.join(__dirname, "..", "public")));
 
   // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  app.use(
+    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
+  );
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 7 días
+      store: MongoStore.create({
+        mongoUrl:
+          process.env.MONGODB_URI || "mongodb://localhost:27017/basic-auth",
+        ttl: 60 * 60 * 24 * 7, // 7 días
+      }),
+    })
+  );
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,68 +2,108 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 
-
-
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup-form.hbs")
-})
+  res.render("auth/signup-form.hbs");
+});
 
 router.post("/signup", async (req, res, next) => {
-    const {username, email, password} = req.body
+  const { username, email, password } = req.body;
 
-    // All fields are filled
-    if(username === "" || email === "" || password === ""){
-        res.status(401).render("auth/signup-form.hbs", {
-            error: "All fields must be filled"
-        })
-        return
+  // All fields are filled
+  if (username === "" || email === "" || password === "") {
+    res.render("auth/signup-form.hbs", {
+      errorMessage: "All fields must be filled",
+    });
+    return;
+  }
+
+  // Password requirements are met
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if (passwordRegex.test(password) === false) {
+    res.render("auth/signup-form.hbs", {
+      errorMessage:
+        "Password must be at least 8 characters long, include upper case letters and a special character",
+    });
+    return;
+  }
+
+  try {
+    // Username does not exist in DB
+    const foundUsername = await User.findOne({ username: username });
+    if (foundUsername !== null) {
+      res.render("auth/signup-form.hbs", {
+        errorMessage: "Username already exists",
+      });
+      return;
     }
 
-    // Password requirements are met
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
-    if(passwordRegex.test(password) === false){
-        res.render("auth/signup-form.hbs", {
-            errorMessage: "Password must be at least 8 characters long, include upper case letters and a special character"
-        })
-        return
+    // Email does not exist in DB
+    const foundEmail = await User.findOne({ email: email });
+    if (foundEmail !== null) {
+      res.render("auth/signup-form.hbs", {
+        errorMessage: "Email already exists",
+      });
+      return;
     }
 
-    try {
+    // Encrypt password
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-        // Username does not exist in DB
-        const foundUsername = await User.findOne({username: username})
-        if(foundUsername !== null){
-            res.render("auth/signup-form.hbs", {
-                errorMessage: "Username already exists"
-            })
-            return
-        }
+    // Create user
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
 
-        // Email does not exist in DB
-        const foundEmail = await User.findOne({email: email})
-        if(foundEmail !== null){
-            res.render("auth/signup-form.hbs", {
-                errorMessage: "Email already exists"
-            })
-            return
-        }
+    res.redirect("/auth/login");
+  } catch (error) {
+    next(error);
+  }
+});
 
-        // Encrypt password
-        const salt = await bcrypt.genSalt(12)
-        const hashedPassword = await bcrypt.hash(password, salt)
+router.get("/login", (req, res, next) => {
+  res.render("auth/login-form.hbs");
+});
 
-        // Create user
-        await User.create({
-            username,
-            email,
-            password: hashedPassword
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
 
-        })
-
-        res.redirect("/auth/login")
-    } catch (error) {
-        next(error)
+  if (email === "" || password === "") {
+    res.render("auth/login-form.hbs", {
+      errorMessage: "The fields must be filled.",
+    });
+  }
+  //validar usuario
+  try {
+    const foundUser = await User.findOne({ email: email });
+    if (foundUser === null) {
+      res.render("auth/login-form.hbs", {
+        errorMessage: "User does not exist.",
+      });
+      return;
     }
-})
+    //verificar contraseña
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      foundUser.password
+    );
+    if (isPasswordCorrect === false) {
+      res.render("auth/login-form.hbs", {
+        errorMessage: "Incorrect password!",
+      });
+      return;
+    }
+
+    //crear sesión
+    req.session.activeUser = foundUser;
+    req.session.save(() => {
+      res.redirect("/profile");
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
